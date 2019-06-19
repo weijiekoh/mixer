249edfeac3aabac0ad5a8badd90e778c1e9f8828
Koh Wei Jie <weijie.koh@consensys.net>
2019-06-19 19:40:47 +0800

    created simple deposit page with text and a deposit button

--- frontend/js/routes/deposit.tsx
+++ frontend/js/routes/deposit.tsx
@@ -3,7 +3,34 @@ import ReactDOM from 'react-dom'
 
 const DepositRoute = () => {
     return (
-        <p>Deposit</p>
+        <div className='section'>
+            <div className='columns has-text-centered'>
+                <div className='column is-12'>
+                    <div className='section'>
+                        <h2 className='subtitle'>
+                            MultiMix (working name) helps you hide your bags.
+                        </h2>
+                    </div>
+                    <div className='section'>
+                        <p>
+                            You can mix 0.1 ETH at a time.
+                        </p>
+                        <p>
+                            The operator's fee is 1%.
+                        </p>
+                        <p>
+                            You'll get back 0.099 ETH at midnight, UTC.
+                        </p>
+                    </div>
+
+                    <div className='section'>
+                        <a className="button is-primary is-large">
+                            Deposit 0.1 ETH
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
