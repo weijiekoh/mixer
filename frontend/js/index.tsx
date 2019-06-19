1084c80522d0ad164c09c17541d2a4d60997cb6b
Koh Wei Jie <weijie.koh@consensys.net>
2019-06-19 18:25:29 +0800

    Refactored the navbar into its own .tsx file

--- frontend/js/index.tsx
+++ frontend/js/index.tsx
@@ -1,60 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Nav from './nav'
 import DepositRoute from './routes/deposit'
 
 const App = () => {
-    const [menuToggle, setMenuToggle] = useState(false)
-
-    const burgerClassName = menuToggle ? 'navbar-burger is-active' : 'navbar-burger'
-    const navbarMenuClassName = menuToggle ? 'navbar-menu is-active' : 'navbar-menu'
-
     return (
         <div className='section'>
-            <nav className='navbar' role='navigation' aria-label='main navigation'>
-                <div className='navbar-brand'>
-                    <a className='navbar-item' href='/'>
-                        MultiMix
-                    </a>
 
-                    <a role='button' className={burgerClassName} 
-                        onClick={ () => setMenuToggle(!menuToggle) }
-                        aria-label='menu' aria-expanded='false'>
-                        <span aria-hidden='true'></span>
-                        <span aria-hidden='true'></span>
-                        <span aria-hidden='true'></span>
-                    </a>
-
-                </div>
-
-                <div className={ navbarMenuClassName }>
-                    <div className='navbar-end'>
-                        <div className='navbar-item' id='wallet-widget'>
-                            Wallet widget
-                        </div>
-
-                        <a className='navbar-item' href='/about'>
-                            About
-                        </a>
-
-                        <div className='navbar-item has-dropdown is-hoverable'>
-                            <a className='navbar-link' id='options-link'>
-                                Options
-                            </a>
-
-                            <div className='navbar-dropdown'>
-                                <a className='navbar-item' href='/quick-withdraw'>
-                                    Quick withdrawal
-                                </a>
-
-                                <a className='navbar-item' href='/backup-keys'>
-                                    Backup keys
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </nav>
+            <Nav />
 
             <div className='section'>
                 <div className='container'>
---
+++ frontend/js/nav.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Nav = () => {
+    const [menuToggle, setMenuToggle] = useState(false)
+
+    const burgerClassName = menuToggle ? 'navbar-burger is-active' : 'navbar-burger'
+    const navbarMenuClassName = menuToggle ? 'navbar-menu is-active' : 'navbar-menu'
+
+    return (
+        <nav className='navbar' role='navigation' aria-label='main navigation'>
+            <div className='navbar-brand'>
+                <a className='navbar-item' href='/'>
+                    MultiMix
+                </a>
+
+                <a role='button' className={burgerClassName} 
+                    onClick={ () => setMenuToggle(!menuToggle) }
+                    aria-label='menu' aria-expanded='false'>
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                </a>
+
+            </div>
+
+            <div className={ navbarMenuClassName }>
+                <div className='navbar-end'>
+                    <div className='navbar-item' id='wallet-widget'>
+                        Wallet widget
+                    </div>
+
+                    <a className='navbar-item' href='/about'>
+                        About
+                    </a>
+
+                    <div className='navbar-item has-dropdown is-hoverable'>
+                        <a className='navbar-link' id='options-link'>
+                            Options
+                        </a>
+
+                        <div className='navbar-dropdown'>
+                            <a className='navbar-item' href='/quick-withdraw'>
+                                Quick withdrawal
+                            </a>
+
+                            <a className='navbar-item' href='/backup-keys'>
+                                Backup keys
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav
