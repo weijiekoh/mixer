d2244958fdc1f76087fd15a8fbc5f3ec75c6561e
Koh Wei Jie <weijie.koh@consensys.net>
2019-06-21 19:12:40 +0800

    got a basic wallet widget working

--- frontend/js/components/walletWidget.tsx
+++ frontend/js/components/walletWidget.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { useWeb3Context, Connectors } from 'web3-react'
 
+const walletLearnUrl = 'https://ethereum.org/use/#_3-what-is-a-wallet' +
+    '-and-which-one-should-i-use'
+
 // From Font Awesome
 const circleIcon = (className: string) => (
     <svg viewBox='0 0 512 512' className={'circle-icon ' + className}>
@@ -9,78 +12,57 @@ const circleIcon = (className: string) => (
     </svg>
 )
 
-const renderWidget = (web3Status: any, context, onConnectBtnClick) => {
-    if (web3Status === 'NO_WALLET') {
-        const walletLearnUrl = 'https://ethereum.org/use/#_3-what-is-a-wallet' +
-            '-and-which-one-should-i-use'
-
-        return (
-            <p>
-                { circleIcon('not-ok') }
-                Please install an <a
-                    href={walletLearnUrl} target='blank'>
-                    Ethereum wallet.
-                </a>
-            </p>
-        )
-
-    } else if (web3Status === 'UNSUPPORTED_NETWORK') {
-        return (
-            <p>
-                { circleIcon('warn') }
-                Please connect to the Kovan testnet.
-            </p>
-        )
-    } else if (web3Status === 'LOGGED_OUT') {
-
-        return (
-            <a className='button is-link is-rounded'
-                role='button'
-                onClick={() => {onConnectBtnClick(context)}} >
-                Connect wallet
-            </a>
-        )
-
-    } else {
-        return (
-            <p>
-                <span className='is-family-monospace address'>
-                    { circleIcon('ok') }
-                    { context.account }
-                </span>
-            </p>
-        )
-    }
-}
-
 const WalletWidget = () => {
     const context = useWeb3Context()
-    const [web3Status, setWeb3Status] = useState('')
 
-    const setConnector = (context) => {
-        return context.setConnector('MetaMask', { suppressAndThrowErrors: true })
+    const setConnector = () => {
+        context.setConnector('MetaMask')
     }
 
-    useEffect(() => {
-        if (context.active) {
-            setConnector(context).then(() => {})
-            .catch((error: any) => {
-                if (error.code === 'UNSUPPORTED_NETWORK') {
-                    setWeb3Status(error.code)
-                }
-            })
-        } else {
-            setWeb3Status('LOGGED_OUT')
-        }
-    })
+    setConnector()
 
-    const onConnectBtnClick = () => {
-        // TODO: figure out the ideal state machine behind web3 / metamask
+    const render = () => {
+        // @ts-ignore
+        if (!window.hasOwnProperty('ethereum')) {
+            return (
+                <p>
+                    { circleIcon('fail') }
+                    Please install an <a
+                        href={walletLearnUrl} target='blank'>
+                        Ethereum wallet.
+                    </a>
+                </p>
+            )
+        } else if (context.active && !context.error) {
+            return (
+                <p>
+                    <span className='is-family-monospace address'>
+                        { circleIcon('ok') }
+                        { context.account }
+                    </span>
+                </p>
+            )
+        } else if (context.error != null && context.error.code === 'UNSUPPORTED_NETWORK') {
+            return (
+                <p>
+                    { circleIcon('warn') }
+                    Please connect to the Kovan testnet.
+                </p>
+            )
+        } else {
+            return (
+                <p className='button is-link is-rounded'
+                    role='button'
+                    onClick={() => {setConnector()}} >
+                    Connect wallet
+                </p>
+            )
+        }
     }
 
     return (
         <div id='wallet-widget'>
-            { renderWidget(web3Status, context, onConnectBtnClick) }
+            {render()}
         </div>
     )
 }
