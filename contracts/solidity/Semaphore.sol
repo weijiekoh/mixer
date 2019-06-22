d48baee120722ceb84b5a524b2981c7e553a1817
Koh Wei Jie <contact@kohweijie.com>
2019-06-22 23:24:21 +0800

    semaphore contracts copied-by-script from the semaphore submodule

---
+++ contracts/solidity/Semaphore.sol
@@ -0,0 +1,137 @@
+/*
+ * semaphorejs - Zero-knowledge signaling on Ethereum
+ * Copyright (C) 2019 Kobi Gurkan <kobigurk@gmail.com>
+ *
+ * This file is part of semaphorejs.
+ *
+ * semaphorejs is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semaphorejs is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with semaphorejs.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+pragma solidity >=0.4.21;
+
+import "./verifier.sol";
+import "./MerkleTreeLib.sol";
+
+contract Semaphore is Verifier, MultipleMerkleTree {
+    address public owner;
+
+    uint256 public external_nullifier;
+    uint8 signal_tree_index;
+    uint8 id_tree_index;
+
+    uint8 constant root_history_size = 100;
+    uint256[root_history_size] root_history;
+    uint8 current_root_index = 0;
+
+    mapping (uint => bool) nullifiers_set;
+
+    uint256 reward;
+
+    event SignalBroadcast(bytes signal, uint256 nullifiers_hash, uint256 external_nullifier);
+
+    uint256 public gas_price_max = 30000000000;
+
+    constructor(uint8 tree_levels, uint256 zero_value, uint256 external_nullifier_in, uint256 reward_amount_in_max_gas_price) public {
+        owner = msg.sender;
+
+        external_nullifier = external_nullifier_in;
+        id_tree_index = init_tree(tree_levels, zero_value);
+        signal_tree_index = init_tree(tree_levels, zero_value);
+
+        reward = reward_amount_in_max_gas_price;
+    }
+
+    event Funded(uint256 amount);
+
+    function fund() public payable {
+      emit Funded(msg.value);
+    }
+
+    modifier onlyOwner() {
+        require (msg.sender == owner);
+        _;
+    }
+
+    function insertIdentity(uint256 leaf) public onlyOwner {
+        insert(id_tree_index, leaf);
+        root_history[current_root_index++ % root_history_size] = tree_roots[id_tree_index];
+    }
+
+    function updateIdentity(uint256 old_leaf, uint256 leaf, uint32 leaf_index, uint256[] memory old_path, uint256[] memory path) public onlyOwner {
+        update(id_tree_index, old_leaf, leaf, leaf_index, old_path, path);
+        root_history[current_root_index++ % root_history_size] = tree_roots[id_tree_index];
+    }
+
+    function broadcastSignal(
+        bytes memory signal,
+        uint[2] a,
+        uint[2][2] b,
+        uint[2] c,
+        uint[5] input // (root, nullifiers_hash, signal_hash, external_nullifier, broadcaster_address)
+    ) public {
+        uint256 start_gas = gasleft();
+
+        uint256 signal_hash = uint256(sha256(signal)) >> 8;
+        require(signal_hash == input[2]);
+        require(external_nullifier == input[3]);
+        require(verifyProof(a, b, c, input));
+        require(nullifiers_set[input[1]] == false);
+        address broadcaster = address(input[4]);
+        require(broadcaster == msg.sender);
+
+        bool found_root = false;
+        for (uint8 i = 0; i < root_history_size; i++) {
+            if (root_history[i] == input[0]) {
+                found_root = true;
+                break;
+            }
+        }
+        require(found_root);
+
+        insert(signal_tree_index, signal_hash);
+        nullifiers_set[input[1]] = true;
+        emit SignalBroadcast(signal, input[1], external_nullifier);
+
+        uint256 gas_price = gas_price_max;
+        if (tx.gasprice < gas_price) {
+          gas_price = tx.gasprice;
+        }
+        uint256 gas_used = start_gas - gasleft();
+
+        // pay back gas: 21000 constant cost + gas used + reward
+        //require((msg.sender).send((21000 + gas_used)*tx.gasprice + reward));
+        require((msg.sender).send((21000 + gas_used + reward)*gas_price));
+        //require(msg.sender.send(1 wei));
+    }
+
+    function roots(uint8 tree_index) public view returns (uint256 root) {
+      root = tree_roots[tree_index];
+    }
+
+    function getIdTreeIndex() public view returns (uint8 index) {
+      index = id_tree_index;
+    }
+
+    function getSignalTreeIndex() public  view returns (uint8 index) {
+      index = signal_tree_index;
+    }
+
+    function setExternalNullifier(uint256 new_external_nullifier) public onlyOwner {
+      external_nullifier = new_external_nullifier;
+    }
+
+    function setMaxGasPrice(uint256 new_max_gas_price) public onlyOwner {
+      gas_price_max = new_max_gas_price;
+    }
+}
---
+++ contracts/solidity/verifier.sol
@@ -0,0 +1,217 @@
+//
+// Copyright 2017 Christian Reitwiessner
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+pragma solidity ^0.4.17;
+library Pairing {
+    struct G1Point {
+        uint X;
+        uint Y;
+    }
+    // Encoding of field elements is: X[0] * z + X[1]
+    struct G2Point {
+        uint[2] X;
+        uint[2] Y;
+    }
+    /// @return the generator of G1
+    function P1() pure internal returns (G1Point) {
+        return G1Point(1, 2);
+    }
+    /// @return the generator of G2
+    function P2() pure internal returns (G2Point) {
+        // Original code point
+        return G2Point(
+            [11559732032986387107991004021392285783925812861821192530917403151452391805634,
+             10857046999023057135944570762232829481370756359578518086990519993285655852781],
+            [4082367875863433681332203403145435568316851327593401208105741076214120093531,
+             8495653923123431417604973247489272438418190587263600148770280649306958101930]
+        );
+
+/*
+        // Changed by Jordi point
+        return G2Point(
+            [10857046999023057135944570762232829481370756359578518086990519993285655852781,
+             11559732032986387107991004021392285783925812861821192530917403151452391805634],
+            [8495653923123431417604973247489272438418190587263600148770280649306958101930,
+             4082367875863433681332203403145435568316851327593401208105741076214120093531]
+        );
+*/
+    }
+    /// @return the negation of p, i.e. p.addition(p.negate()) should be zero.
+    function negate(G1Point p) pure internal returns (G1Point) {
+        // The prime q in the base field F_q for G1
+        uint q = 21888242871839275222246405745257275088696311157297823662689037894645226208583;
+        if (p.X == 0 && p.Y == 0)
+            return G1Point(0, 0);
+        return G1Point(p.X, q - (p.Y % q));
+    }
+    /// @return the sum of two points of G1
+    function addition(G1Point p1, G1Point p2) view internal returns (G1Point r) {
+        uint[4] memory input;
+        input[0] = p1.X;
+        input[1] = p1.Y;
+        input[2] = p2.X;
+        input[3] = p2.Y;
+        bool success;
+        assembly {
+            success := staticcall(sub(gas, 2000), 6, input, 0xc0, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+    }
+    /// @return the product of a point on G1 and a scalar, i.e.
+    /// p == p.scalar_mul(1) and p.addition(p) == p.scalar_mul(2) for all points p.
+    function scalar_mul(G1Point p, uint s) view internal returns (G1Point r) {
+        uint[3] memory input;
+        input[0] = p.X;
+        input[1] = p.Y;
+        input[2] = s;
+        bool success;
+        assembly {
+            success := staticcall(sub(gas, 2000), 7, input, 0x80, r, 0x60)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require (success);
+    }
+    /// @return the result of computing the pairing check
+    /// e(p1[0], p2[0]) *  .... * e(p1[n], p2[n]) == 1
+    /// For example pairing([P1(), P1().negate()], [P2(), P2()]) should
+    /// return true.
+    function pairing(G1Point[] p1, G2Point[] p2) view internal returns (bool) {
+        require(p1.length == p2.length);
+        uint elements = p1.length;
+        uint inputSize = elements * 6;
+        uint[] memory input = new uint[](inputSize);
+        for (uint i = 0; i < elements; i++)
+        {
+            input[i * 6 + 0] = p1[i].X;
+            input[i * 6 + 1] = p1[i].Y;
+            input[i * 6 + 2] = p2[i].X[0];
+            input[i * 6 + 3] = p2[i].X[1];
+            input[i * 6 + 4] = p2[i].Y[0];
+            input[i * 6 + 5] = p2[i].Y[1];
+        }
+        uint[1] memory out;
+        bool success;
+        assembly {
+            success := staticcall(sub(gas, 2000), 8, add(input, 0x20), mul(inputSize, 0x20), out, 0x20)
+            // Use "invalid" to make gas estimation work
+            switch success case 0 { invalid() }
+        }
+        require(success);
+        return out[0] != 0;
+    }
+    /// Convenience method for a pairing check for two pairs.
+    function pairingProd2(G1Point a1, G2Point a2, G1Point b1, G2Point b2) view internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](2);
+        G2Point[] memory p2 = new G2Point[](2);
+        p1[0] = a1;
+        p1[1] = b1;
+        p2[0] = a2;
+        p2[1] = b2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for three pairs.
+    function pairingProd3(
+            G1Point a1, G2Point a2,
+            G1Point b1, G2Point b2,
+            G1Point c1, G2Point c2
+    ) view internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](3);
+        G2Point[] memory p2 = new G2Point[](3);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        return pairing(p1, p2);
+    }
+    /// Convenience method for a pairing check for four pairs.
+    function pairingProd4(
+            G1Point a1, G2Point a2,
+            G1Point b1, G2Point b2,
+            G1Point c1, G2Point c2,
+            G1Point d1, G2Point d2
+    ) view internal returns (bool) {
+        G1Point[] memory p1 = new G1Point[](4);
+        G2Point[] memory p2 = new G2Point[](4);
+        p1[0] = a1;
+        p1[1] = b1;
+        p1[2] = c1;
+        p1[3] = d1;
+        p2[0] = a2;
+        p2[1] = b2;
+        p2[2] = c2;
+        p2[3] = d2;
+        return pairing(p1, p2);
+    }
+}
+contract Verifier {
+    using Pairing for *;
+    struct VerifyingKey {
+        Pairing.G1Point alfa1;
+        Pairing.G2Point beta2;
+        Pairing.G2Point gamma2;
+        Pairing.G2Point delta2;
+        Pairing.G1Point[] IC;
+    }
+    struct Proof {
+        Pairing.G1Point A;
+        Pairing.G2Point B;
+        Pairing.G1Point C;
+    }
+    function verifyingKey() pure internal returns (VerifyingKey vk) {
+        vk.alfa1 = Pairing.G1Point(11372918416185494881300392185874533969159082008431368195522295255139359626956,21669336075217908562513716047860628648189285549369234688482309483632234729879);
+        vk.beta2 = Pairing.G2Point([8285205585098253449662347559145033581153818722425161739531430776394870629588,4745697128084521282031030206718177757545878542736157282075026211734004675505], [9212404736005400347299072802338288191520703905369690408213171948151114546411,267240873700001609802621873475399365068278993708176910492374857259381143708]);
+        vk.gamma2 = Pairing.G2Point([9480928853655186612601602909401934702371710787264004742965112763326328235610,3011533606325510722390692975258880733761779774859261547685700284188576059681], [7771731628194111351195382631967105244099632722424919065534414836772570651950,5534981617787522913993180410593247392993109231081830394839329127849311335760]);
+        vk.delta2 = Pairing.G2Point([10721555624246419592801172719019750817434031701223870736820807246037220517204,12969668529229271269452684687859467740728451216423222805500743098842181199596], [5094631695711214372844590145586197626713973678794152088981530365369621458876,7951037569245093755709448692811585050390184992281107948411945718739847446081]);
+        vk.IC = new Pairing.G1Point[](6);
+        vk.IC[0] = Pairing.G1Point(19015214816541630469957526862173004415904140995152801262641739069196598800977,17869292779032240775635818873566091705205141494108579026809457090107758187149);
+        vk.IC[1] = Pairing.G1Point(1507461442715444285986300272234403565094457037089628873860093240715668778572,9608497618968864939856753407193207939192808358881617335685684554575976529363);
+        vk.IC[2] = Pairing.G1Point(16690647434833019443148489589855919506800518254605333567482073873190256720931,16889868833112318732071797941760556751755405667191112245144510964978751036905);
+        vk.IC[3] = Pairing.G1Point(11696185897894423493931780481712162834134238437426506286573647845794981099412,18453775881747433509571298452607318446470099501421241045942330770886297721470);
+        vk.IC[4] = Pairing.G1Point(4259852061917508510256869666101581641226574311386569946320402104326922615434,21799854223634242947783254710420862102675295919360476298732482152800150343200);
+        vk.IC[5] = Pairing.G1Point(448450498539719199950481833849799398732479913866140287700916457031724618581,2460676507965308220348560553298119593392138257605988290410345148893488090439);
+
+    }
+    function verify(uint[] input, Proof proof) view internal returns (uint) {
+        VerifyingKey memory vk = verifyingKey();
+        require(input.length + 1 == vk.IC.length);
+        // Compute the linear combination vk_x
+        Pairing.G1Point memory vk_x = Pairing.G1Point(0, 0);
+        for (uint i = 0; i < input.length; i++)
+            vk_x = Pairing.addition(vk_x, Pairing.scalar_mul(vk.IC[i + 1], input[i]));
+        vk_x = Pairing.addition(vk_x, vk.IC[0]);
+        if (!Pairing.pairingProd4(
+            Pairing.negate(proof.A), proof.B,
+            vk.alfa1, vk.beta2,
+            vk_x, vk.gamma2,
+            proof.C, vk.delta2
+        )) return 1;
+        return 0;
+    }
+    function verifyProof(
+            uint[2] a,
+            uint[2][2] b,
+            uint[2] c,
+            uint[5] input
+        ) view public returns (bool r) {
+        Proof memory proof;
+        proof.A = Pairing.G1Point(a[0], a[1]);
+        proof.B = Pairing.G2Point([b[0][0], b[0][1]], [b[1][0], b[1][1]]);
+        proof.C = Pairing.G1Point(c[0], c[1]);
+        uint[] memory inputValues = new uint[](input.length);
+        for(uint i = 0; i < input.length; i++){
+            inputValues[i] = input[i];
+        }
+        if (verify(inputValues, proof) == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
