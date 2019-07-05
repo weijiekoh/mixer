d34b37c70a804ce587550d15281b8e9aea0dcaf9
Koh Wei Jie <contact@kohweijie.com>
2019-07-05 22:45:38 +0800

    frontend watch script that proxies localhost:8000 for downloading the circuit and proving key via fetch

---
+++ frontend/server.js
@@ -0,0 +1,32 @@
+const ParcelProxyServer = require('parcel-proxy-server');
+
+// configure the proxy server
+const server = new ParcelProxyServer({
+  entryPoint: './index.html',
+  parcelOptions: {
+    // provide parcel options here
+    // these are directly passed into the
+    // parcel bundler
+    //
+    // More info on supported options are documented at
+    // https://parceljs.org/api
+    https: false,
+    autoinstall: false,
+  },
+  proxies: {
+    '/build': {
+      target: 'http://localhost:8000/'
+    }
+  }
+});
+
+// the underlying parcel bundler is exposed on the server
+// and can be used if needed
+server.bundler.on('buildEnd', () => {
+  console.log('Build completed!');
+});
+
+// start up the server
+server.listen(1234, () => {
+  console.log('Parcel proxy server has started');
+});
