5028ad4f1e1dee250a358afa6bdebda3b94b539c
Koh Wei Jie <contact@kohweijie.com>
2019-07-08 17:29:00 +0800

    updated semaphore

--- scripts/downloadSnarks.sh
+++ scripts/downloadSnarks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir -p semaphore/semaphorejs/build
-wget -nc https://www.dropbox.com/s/wnav410x4zuox4z/circuit.json?dl=1 -O semaphore/semaphorejs/build/circuit.json
-wget https://www.dropbox.com/s/zgi99x7peevfzhb/proving_key.bin?dl=1 -nc  -O semaphore/semaphorejs/build/proving_key.bin
-wget https://www.dropbox.com/s/vka5jqcacy9k86h/proving_key.json?dl=1 -nc  -O semaphore/semaphorejs/build/proving_key.json
-wget -nc https://www.dropbox.com/s/3359a7c2ykhhj98/verification_key.json?dl=1 -O semaphore/semaphorejs/build/verification_key.json
-wget -nc https://www.dropbox.com/s/nuth00m5nnue122/verifier.sol?dl=1 -O semaphore/semaphorejs/build/verifier.sol
+wget -nc https://www.dropbox.com/s/93fr9wmv2ynjsah/circuit.json?dl=1 -O semaphore/semaphorejs/build/circuit.json
+wget -nc https://www.dropbox.com/s/1xv2k21h75s8esc/proving_key.bin?dl=1 -O semaphore/semaphorejs/build/proving_key.bin
+wget -nc https://www.dropbox.com/s/a2ryrbnfk43q25l/proving_key.json?dl=1 -O semaphore/semaphorejs/build/proving_key.json
+wget -nc https://www.dropbox.com/s/5qrvbnv5yakf7wn/verification_key.json?dl=1 -O semaphore/semaphorejs/build/verification_key.json
+wget -nc https://www.dropbox.com/s/e095d2m8hlxoen7/verifier.sol?dl=0 -O semaphore/semaphorejs/build/verifier.sol
