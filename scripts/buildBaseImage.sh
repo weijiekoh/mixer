58bf05d479a1913d020292af7f276e1f5e460671
Koh Wei Jie <contact@kohweijie.com>
2019-07-16 04:30:00 +0800

    tweaked docker scripts

--- scripts/buildBaseImage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -f Dockerfile -t mixer-build --target mixer-build --build-arg NODE_ENV=$NODE_ENV .
-#docker build -f Dockerfile -t mixer-base --target mixer-base .
--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose -f docker/docker-compose.dev.yml --build
+docker build -f Dockerfile -t mixer-build --target mixer-build --build-arg NODE_ENV=$NODE_ENV .
+docker build -f Dockerfile -t mixer-base --target mixer-base .
+docker-compose -f docker/docker-compose.dev.yml build
