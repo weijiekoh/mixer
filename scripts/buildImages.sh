cf08a7b4384db4fd1bd360e5b862684cbc4b9b98
Koh Wei Jie <contact@kohweijie.com>
2019-07-16 04:50:40 +0800

    echo progress when building images

--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Building mixer-build"
 docker build -f Dockerfile -t mixer-build --target mixer-build --build-arg NODE_ENV=$NODE_ENV .
+
+echo "Building mixer-base"
 docker build -f Dockerfile -t mixer-base --target mixer-base .
+
+echo "Building images using docker-compose"
 docker-compose -f docker/docker-compose.dev.yml build
