9a659ee57a5984b4638ffb793332eb0896b32cca
Koh Wei Jie <contact@kohweijie.com>
2019-07-16 05:23:54 +0800

    fix to buildImages.sh

--- scripts/buildImages.sh
+++ scripts/buildImages.sh
@@ -4,7 +4,7 @@ echo "Building mixer-build"
 docker build -f Dockerfile -t mixer-build --target mixer-build --build-arg NODE_ENV=$NODE_ENV .
 
 echo "Building mixer-base"
-docker build -f Dockerfile -t mixer-base --target mixer-base .
+docker build -f Dockerfile -t mixer-base --target mixer-base --build-arg NODE_ENV=$NODE_ENV .
 
 echo "Building images using docker-compose"
 docker-compose -f docker/docker-compose.dev.yml build
