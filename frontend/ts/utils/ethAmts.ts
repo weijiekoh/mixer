39eff59d2f4eeb41b659c7ccb1c19767f642e7b4
Koh Wei Jie <contact@kohweijie.com>
2019-07-24 13:36:18 +0800

    backend dockerfile - remove frontend files; ignore local config

--- frontend/ts/utils/ethAmts.ts
+++ frontend/ts/utils/ethAmts.ts
@@ -3,7 +3,11 @@ import * as ethers from 'ethers'
 const config = require('../exported_config')
 
 const mixAmtEth = config.mixAmtEth
+
+// the operator is the same as the burn feee, a choice arbitarily made by
+// this relayer
 const operatorFeeEth = config.burnFeeEth
+
 const feeAmtWei = ethers.utils.parseEther(
     (parseFloat(operatorFeeEth) * 2).toString()
 )
