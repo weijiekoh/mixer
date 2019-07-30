e3e25ffef7df072985f7768cb3279891d6aef45e
Koh Wei Jie <contact@kohweijie.com>
2019-07-30 17:12:06 +0800

    fix to contract test index.d.ts

--- contracts/ts/__tests__/index.d.ts
+++ contracts/ts/__tests__/index.d.ts
@@ -2,5 +2,5 @@ declare var accounts: any[]
 declare var utils: any
 declare var assert: any
 declare var before: any
-declare var it: any
-declare var describe: any
+//declare var it: any
+//declare var describe: any
